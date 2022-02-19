b9344879a0e6905e57a8653c844d711cff06201d
hogehoge <hogehoge@gmail.com>
2022-02-19 15:05:58 +0900

    追跡ゲーム

---
+++ Assets/Scripts/PlayerController1.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController1 : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.Rotate(0, Input.GetAxis("Horizontal"), 0);
+        transform.Translate(0, 0, Input.GetAxis("Vertical") * 5.0f * Time.deltaTime);
+    }
+
+}
+
---
+++ Assets/Scripts/PlayerController2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour
+{
+	private CharacterController cc;
+	void Start()
+	{
+		cc = GetComponent<CharacterController>();
+	}
+	void Update()
+	{
+		transform.Rotate(Vector3.up * Input.GetAxis("Horizontal"));
+		cc.SimpleMove(transform.forward * Input.GetAxis("Vertical") * Time.deltaTime * 220f);
+	}
+}
